1955ac8b74f2c570bbf9002216aaacc86a31ae9b
Bartek Ciszewski <bartektricks@gmail.com>
2018-10-10 20:30:04 +0200

    fix

--- src/components/ElementsList/ElementsList.js
+++ src/components/ElementsList/ElementsList.js
@@ -4,22 +4,18 @@ import list from './ExternalList';
 
 function listElement() {
     const listItems = Object.keys(list);
+    const displayList = listItems.map((e, i) => ( 
+        <li className = 'listElement' key = {i} > {e} </li>
+    ));
 
-    listItems.map((element) => {
-        console.log(element);
-        return (
-            <li className='listElement' key={new Date()} >
-                {element}
-            </li>
-        );
-    })
+    return displayList;
 }
 
 class ElementsList extends Component {
     render() {
-        return (
-            <ul className='elementsList'>
-                {listElement()}
+        return ( 
+            <ul className = 'elementsList' > 
+                {listElement()} 
             </ul>
         );
     }
